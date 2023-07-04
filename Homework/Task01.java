590ca01a94998bc4f119a4bcc1f601058b51427a
Andrew_Kochubeeev <andrew-k-44@mail.ru>
2023-07-04 18:07:21 +0300

    Task01

--- Homework/Task01.java
+++ Homework/Task01.java
@@ -6,6 +6,13 @@ package Homework;
 public class Task01 {
 
     public static void main(String[] args) {
-        
+        String sqlString = "select * from students where ";
+        // String jsonFilter =  {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+        StringBuilder sb = new StringBuilder(sqlString);
+        sb.append("\"name\" = \"Ivanov\" ");
+        sb.append("and \"country\" = \"Russia\" ");
+        sb.append("and \"city\" = \"Moscow\"");
+        System.out.println(sb.toString());
+
     }
 }
